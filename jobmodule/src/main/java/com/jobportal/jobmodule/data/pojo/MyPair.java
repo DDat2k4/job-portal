eb4279da510e459b79c107c046474f35ece074af
Dinh Dat <tddat19102004@gmail.com>
2025-10-01 16:26:06 +0700

    add commonutil

--- jobmodule/src/main/java/com/jobportal/jobmodule/data/pojo/MyPair.java
+++ jobmodule/src/main/java/com/jobportal/jobmodule/data/pojo/MyPair.java
@@ -1,4 +1,15 @@
-package com.jobportal.jobmodule.data;
+package com.jobportal.jobmodule.data.pojo;
 
-public class MyPair {
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class MyPair<T, V> {
+    T key;
+    V value;
+    public MyPair(T key, V value) {
+        this.key = key;
+        this.value = value;
+    }
 }
